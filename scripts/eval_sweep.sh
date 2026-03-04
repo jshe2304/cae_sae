2af574b86244a870e4b743db9a3a10d54fd353b7
Jay Shen <jshen6@br011.ib.bridges2.psc.edu>
2026-03-04 18:00:26 -0500

    fixed slurm args

--- scripts/eval_sweep.sh
+++ scripts/eval_sweep.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=eval_sweep
-#SBATCH --partition=gpu
-#SBATCH --gres=gpu:1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=32G
-#SBATCH --time=04:00:00
-#SBATCH --output=/ocean/projects/atm170004p/jshen6/cae_sae/slurm_logs/eval_sweep_%j.out
-#SBATCH --error=/ocean/projects/atm170004p/jshen6/cae_sae/slurm_logs/eval_sweep_%j.err
+#SBATCH -A atm170004p
+#SBATCH -t 04:00:00
+#SBATCH -p GPU-shared
+#SBATCH --gpus=v100-16:1
 
 python -m scripts.eval_sweep
